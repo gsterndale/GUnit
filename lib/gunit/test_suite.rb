22cf504232c2f00cb41ff668a735403264a6d913
Greg <greg@plectix.com>
2010-01-30 16:30:08 -0500

    TestSuites do not run() tests

--- lib/gunit/test_suite.rb
+++ lib/gunit/test_suite.rb
@@ -1,29 +1,17 @@
 module GUnit
-  
+
   class TestSuite
-    
-    # TestCases (TODO and/or TestSuites)
+
+    # TestCases and/or TestSuites
     attr_writer :tests
-    
+
     def initialize()
     end
-    
+
     def tests
       @tests ||= []
     end
-    
-    def run(&blk)
-      self.tests.each do |test|
-        case
-        when test.is_a?(TestSuite)
-          test.run{|response| blk.call(response) if blk }
-        when test.is_a?(TestCase)
-          response = test.run
-          blk.call(response) if blk
-        end
-      end
-    end
-    
+
   end
-  
+
 end
--- test/unit/test_suite_test.rb
+++ test/unit/test_suite_test.rb
@@ -1,48 +1,26 @@
 require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
 class GUnit::TestSuiteTest < Test::Unit::TestCase
-  
+
   def setup
     @test_suite = GUnit::TestSuite.new
   end
-  
+
   def test_it_is_a_test_suite
     assert @test_suite.is_a?(GUnit::TestSuite)
   end
-  
+
   def test_tests_getter_setter
     tests = [GUnit::TestSuite.new, GUnit::TestCase.new]
     assert @test_suite.tests != tests
     @test_suite.tests = tests
     assert @test_suite.tests == tests
   end
-  
+
   def test_tests_set_nil_has_empty_tests
     @test_suite.tests = nil
     assert @test_suite.tests.empty?
     assert @test_suite.tests.is_a?(Enumerable)
   end
-  
-  def test_run_runs_all_tests_saves_responses
-    test_response1 = GUnit::PassResponse.new
-    test_response2 = GUnit::FailResponse.new
-    test_response3 = GUnit::ExceptionResponse.new
 
-    test_suite = GUnit::TestSuite.new
-    test_suite.expects(:run).multiple_yields(test_response1, test_response2)
-
-    test_case = GUnit::TestCase.new
-    test_case.expects(:run).returns(test_response3)
-
-    @test_suite.tests = [test_suite, test_case]
-
-    responses = []
-    @test_suite.run{|r| responses << r }
-    
-    assert responses.include?(test_response1)
-    assert responses.include?(test_response2)
-    assert responses.include?(test_response3)
-    assert responses.length == 3
-  end
-  
 end
